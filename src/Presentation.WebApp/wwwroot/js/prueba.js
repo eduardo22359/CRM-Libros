81e96df4b1d5abda023c320d26bed29e636a89a7
Eduardo Adame <eduardo.adame@my.unitec.edu.mx>
2023-03-28 15:27:42 -0600

    prueba

--- src/Presentation.WebApp/wwwroot/js/prueba.js
+++ src/Presentation.WebApp/wwwroot/js/prueba.js
@@ -16,5 +16,23 @@ $(document).ready(function () {
             }
 
         }
+    ),
+    $('#suiddeustedes').DataTable(
+        {
+            "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
+            /* Cambia el color de la fila al pasar el mouse */
+            "rowCallback": function (row, data, index) {
+                $(row).hover(
+                    function () {
+                        $(this).addClass('highlight');
+                    },
+                    function () {
+                        $(this).removeClass('highlight');
+                    }
+                );
+            }
+
+        }
     );
 });
+
