6da2898328897b575f20051a4acfb94871441ed6
alan25693 <alan.salgadoc@my.unitec.edu.mx>
2023-03-28 17:49:52 -0600

    mitrabajohaterminado

--- src/Presentation.WebApp/wwwroot/js/prueba.js
+++ src/Presentation.WebApp/wwwroot/js/prueba.js
@@ -33,6 +33,23 @@ $(document).ready(function () {
             }
 
         }
+    ),
+    $('#tablaEmpleados').DataTable(
+        {
+            "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
+            /* Cambia el color de la fila al pasar el mouse */
+            "rowCallback": function (row, data, index) {
+                $(row).hover(
+                    function () {
+                        $(this).addClass('highlight');
+                    },
+                    function () {
+                        $(this).removeClass('highlight');
+                    }
+                );
+            }
+
+        }
     );
 });
 
