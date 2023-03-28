df425dae4ec52ad499a0ffb7f0a378e1880e1955
Eduardo Adame <eduardo.adame@my.unitec.edu.mx>
2023-03-28 15:24:57 -0600

    tabla clientes

--- src/Presentation.WebApp/wwwroot/js/prueba.js
+++ src/Presentation.WebApp/wwwroot/js/prueba.js
@@ -1,4 +1,20 @@
 //Se utiliza DataTables para darle formato
 $(document).ready(function () {
-    $('#tablaClientes').DataTable();
+    $('#tablaClientes').DataTable(
+        {
+            "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
+            /* Cambia el color de la fila al pasar el mouse */
+            "rowCallback": function (row, data, index) {
+                $(row).hover(
+                    function () {
+                        $(this).addClass('highlight');
+                    },
+                    function () {
+                        $(this).removeClass('highlight');
+                    }
+                );
+            }
+
+        }
+    );
 });
