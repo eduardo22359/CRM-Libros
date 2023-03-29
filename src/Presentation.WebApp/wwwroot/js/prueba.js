79744fc7264d04ed2d51e237b934a67372a38187
Eduardo Adame <eduardo.adame@my.unitec.edu.mx>
2023-03-28 18:52:46 -0600

    código modificado y acelerado

--- src/Presentation.WebApp/wwwroot/js/prueba.js
+++ src/Presentation.WebApp/wwwroot/js/prueba.js
@@ -1,55 +1,29 @@
-//Se utiliza DataTables para darle formato
 $(document).ready(function () {
-    $('#tablaClientes').DataTable(
-        {
-            "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
-            /* Cambia el color de la fila al pasar el mouse */
-            "rowCallback": function (row, data, index) {
-                $(row).hover(
-                    function () {
-                        $(this).addClass('highlight');
-                    },
-                    function () {
-                        $(this).removeClass('highlight');
-                    }
-                );
-            }
 
-        }
-    ),
-    $('#tablaProductos').DataTable(
-        {
+    // Función para crear una tabla con DataTables y configurar los botones
+    function crearTabla(tablaId) {
+        $(tablaId).DataTable({
             "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
-            /* Cambia el color de la fila al pasar el mouse */
-            "rowCallback": function (row, data, index) {
-                $(row).hover(
-                    function () {
-                        $(this).addClass('highlight');
-                    },
-                    function () {
-                        $(this).removeClass('highlight');
-                    }
-                );
-            }
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    autoFilter: true,
+                    sheetName: 'Tabla'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL'
+                },
+                'print',
+                'colvis'
+            ]
+        });
+    }
 
-        }
-    ),
-    $('#tablaEmpleados').DataTable(
-        {
-            "language": { "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json" },
-            /* Cambia el color de la fila al pasar el mouse */
-            "rowCallback": function (row, data, index) {
-                $(row).hover(
-                    function () {
-                        $(this).addClass('highlight');
-                    },
-                    function () {
-                        $(this).removeClass('highlight');
-                    }
-                );
-            }
-
-        }
-    );
+    // Crear tablas y configurar los botones
+    crearTabla('#tablaClientes');
+    crearTabla('#tablaProductos');
+    crearTabla('#tablaEmpleados');
 });
-
