b266dc2add9c4d00070250645f5f0b0125d0dd59
Eduardo Adame <eduardo.adame@my.unitec.edu.mx>
2023-03-28 20:26:13 -0600

    Ventana Confirmar para eliminar lista

--- src/Presentation.WebApp/wwwroot/js/prueba.js
+++ src/Presentation.WebApp/wwwroot/js/prueba.js
@@ -1,5 +1,50 @@
 $(document).ready(function () {
 
+    $('.eliminar-btn').click(function (e) {
+        e.preventDefault();
+        // Mostrar SweetAlert
+        var botonEliminar = $(this);
+        Swal.fire({
+            title: '¿Está seguro que desea eliminar este registro?',
+            text: "Esta acción no se puede deshacer",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Enviar una solicitud de eliminación al servidor
+                $.ajax({
+                    url: botonEliminar.attr('href'),
+                    type: 'POST',
+                    data: {
+                        id: botonEliminar.data('id')
+                    },
+                    success: function (response) {
+                        // Mostrar SweetAlert de éxito
+                        Swal.fire(
+                            'Eliminado!',
+                            'El registro ha sido eliminado.',
+                            'success'
+                        )
+
+                        botonEliminar.closest('tr').remove();
+
+                    },
+                    error: function (error) {
+                        // Mostrar SweetAlert de error
+                        Swal.fire(
+                            'Error!',
+                            'Ocurrió un error al intentar eliminar el registro debido a que pertence a otras tablas.',
+                            'error'
+                        )
+                    }
+                });
+            };
+        });
+    });
+
     // Función para crear una tabla con DataTables y configurar los botones
     function crearTabla(tablaId) {
         $(tablaId).DataTable({
